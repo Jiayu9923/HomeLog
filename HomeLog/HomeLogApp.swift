58a29e39339a07ecc621e33efa105fe388feb982
Jiayu Huang <jiayu9923@gmail.com>
2023-12-27 01:40:37 +1100

    Initial Commit

---
+++ HomeLog/HomeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeLogApp.swift
+//  HomeLog
+//
+//  Created by 黄佳钰 on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct HomeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
